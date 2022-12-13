0aa0835a10d425f3437e9e50f9c23d1cefc2749e
diego sandes <diegosandes88.dsgmail>
2022-12-12 21:25:15 -0300

    se modifico los menu mobiles

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email')
 const menuDesktop = document.querySelector('.desktop-menu')
+const menuIcon= document.querySelector('.menuIcon')
+const menuMobile =document.querySelector('.mobile-menu')
 
 menuEmail.addEventListener('click',toggledesktopMenu)
 
 function toggledesktopMenu(){
     menuDesktop.classList.toggle('inactive')
+}
+
+menuIcon.addEventListener('click',toggleMenuMobile)
+
+function toggleMenuMobile(){
+    menuMobile.classList.toggle('inactive')
 }
