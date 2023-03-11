3829a3e0e2144ffefda7012f72e814e62a98ea61
diego sandes <diegosandes88.dsgmail>
2023-03-11 20:29:35 -0300

    modificacion de los menus

--- main.js
+++ main.js
@@ -1,16 +1,45 @@
 const menuEmail = document.querySelector('.navbar-email')
-const menuDesktop = document.querySelector('.desktop-menu')
 const menuIcon= document.querySelector('.menuIcon')
-const menuMobile =document.querySelector('.mobile-menu')
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const menuDesktop = document.querySelector('.desktop-menu')
+const menuMobile = document.querySelector('.mobile-menu')
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click',toggledesktopMenu)
+menuIcon.addEventListener('click',toggleMenuMobile)
+menuCarritoIcon.addEventListener('click',toggleCarritoAside)
+
 
 function toggledesktopMenu(){
+    const isAsideMenuClosed = aside.classList.contains('inactive')
+
+    if(!isAsideMenuClosed){
+        aside.classList.add('inactive')
+    }
+
     menuDesktop.classList.toggle('inactive')
+
 }
 
-menuIcon.addEventListener('click',toggleMenuMobile)
 
 function toggleMenuMobile(){
+    const isAsideMenuClosed = aside.classList.contains('inactive')
+
+    if(!isAsideMenuClosed){
+        aside.classList.add('inactive')
+    }
+
     menuMobile.classList.toggle('inactive')
+}
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed = menuMobile.classList.contains('inactive')
+    const isMenuDesktopClosed = menuDesktop.classList.contains('inative')
+
+    if(!isMobileMenuClosed || !isMenuDesktopClosed){
+        menuMobile.classList.add('inactive')
+        menuDesktop.classList.add('inactive')
+    }
+
+    aside.classList.toggle('inactive')
 }
